21868f4bcf2059fd28a27006d88ad6d3a8ff8325
Hakim Nizami <hakimnizami15@gmail.com>
2024-05-16 10:51:52 +0700

    masi bingung

---
+++ ModeMahasiswa/Menu.h
@@ -0,0 +1,10 @@
+void Menu() {
+    /*
+    Nama Mahasiswa
+    NPM
+
+    1. Pilih Mata Kuliah
+    2. Lihat Status UKT
+    3. Riwayat Nilai
+    */
+}
---
+++ PAS-01.c
@@ -0,0 +1,32 @@
+/*
+Proyek Akhir Semester - Pemrograman Lanjut 02
+Program Simulasi SIAK Sederhana
+
+Anggota Kelompok 1:
+1. Daffa Sayra Firdaus (2306267151)
+2. Muhamad Hakim Nizami (2306210001)
+
+Versi Program : 1.0
+
+Deskripsi:
+Tujuan dari program adalah untuk melakukan olah data mahasiswa untuk grading individu, ataupun grading keseluruhan mahasiswa beserta status mereka sebagai simulasi SIAK sederhana dengan fitur-fitur sebagai berikut:
+1. Mode Dosen
+- Program menerima input data nilai mahasiswa
+- Menghitung data tendencies dari seluruh data nilai mahasiswa
+- Mengkonfirmasi Status keaktifan masing-masing mahasiswa 
+- Mempersetujui matkul yang dipilih oleh mahasiswa berdasarkan pertimbangan batas SKS dan persyaratan nilai/IP
+- Menentukan jika mahasiswa lulus atau tidak
+- Menampilkan bagan nilai mahasiswa (tampilan secara kasar)
+- Multithreading digunakan untuk menjalankan hitungan program pada jumlah data yang sangat besar untuk meningkatkan efisiensi (>50.000 data)
+
+2. Mode Mahasiswa
+- Memilih matakuliah/kelas yang dipelajari untuk satu semester
+- Membayar UKT untuk menjaga status aktif sebagai mahasiswa
+- Melihat Chart/Data nilai semester/semasa kuliah
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+}
